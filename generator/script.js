fe7ebd6d5f1706a786ed2812077ae2d36f1abab0
ysamda <219377002+ysamda@users.noreply.github.com>
2025-08-04 22:25:12 +0900

    Update script.js

--- generator/script.js
+++ generator/script.js
@@ -1,5 +1,5 @@
 const canvas   = document.getElementById('memeCanvas');
-const ctx      = canvas.getcontext('2d');
+const ctx      = canvas.getContext('2d');
 const charSel  = document.getElementById('characterSelect');
 const topIn    = document.getElementById('topText');
 const botIn    = document.getElementById('bottomText');
@@ -8,35 +8,34 @@ const dlBtn    = document.getElementById('downloadBtn');
 
 function drawMeme() {
     const img  = new Image();
-    img.src = 'imgages/${charSel.value}';
+    img.src = `images/${charSel.value}`;
     img.onload = () => {
         ctx.clearRect(0,0,canvas.width, canvas.height);
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-        ctx.fillstyle = 'white';
-        ctx.stroketype = 'black';
-        ctx.linewidth = 4;
+        ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 4;
         ctx.textAlign = 'center';
-        ctx.front    = 'bold 30px sans-serif';
+        ctx.font    = 'bold 30px sans-serif';
 
-        ctx.filltext(topIn.value, canvas.width/2,40);
-        ctx.stroketext(topIn.value, canvas.width/2, 40);
+        ctx.fillText(topIn.value, canvas.width/2,40);
+        ctx.strokeText(topIn.value, canvas.width/2, 40);
 
 
-        ctx.filltext(botIn.value, canvas.width/2, canvas.height -20);
-        ctx.stroketext(botIn.value, canvas.width/2, canvas.height - 20);
+        ctx.fillText(botIn.value, canvas.width/2, canvas.height -20);
+        ctx.strokeText(botIn.value, canvas.width/2, canvas.height - 20);
 
         dlBtn.style.display  = 'inline-block';
 
     };
-
-    getBtn.adEventListener('click',drawMeme);
+}
+    genBtn.addEventListener('click',drawMeme);
 
     dlBtn.addEventListener('click',()=>{
-        link.download ="3dwojak-meme.png";
+        const link = document.createElement('a');
+        link.download ='3dwojak-meme.png';
         link.href     = canvas.toDataURL('image/png');
         link.click();
 
     });
-    
-}
